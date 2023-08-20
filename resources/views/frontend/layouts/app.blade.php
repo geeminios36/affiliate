05d10819a60c900e2037795186c025d14d4c77ba
Nam Tong <41351528+namth3061@users.noreply.github.com>
2023-08-20 19:49:42 +0700

    Update app.blade.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -81,6 +81,11 @@
     <!-- Main Style CSS -->
     <link rel="stylesheet" href="{{  static_asset('assets/frontend/css/style.css') }}">
 
+    {{-- Link toastr --}}
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     @php
         echo get_setting('header_script');
